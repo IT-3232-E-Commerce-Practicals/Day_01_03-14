52ff5c0fe96bcf774208f34c1d02aa9e8c17d882
Bingusala <139640476+Bingusala@users.noreply.github.com>
2025-03-15 17:48:27 +0000

    Added PrintedBook class extending Book with popular page feature and display details method

---
+++ Printedbook.java
@@ -0,0 +1,19 @@
+
+public class Printedbook extends Book {
+    private int popularpage;
+
+    public PrintedBook(String title, String author, int publish_year, int popularpage) {
+        super(title, author, publish_year);
+        this.popularpage = popularpage;
+    }
+
+    public int getPopularpage() {
+        return popularpage;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Printed Book: " + getTitle() + " by " + getAuthor() + 
+                           ", Year: " + getPublicationYear() + ", Popular Pages: " + popularpage);
+    }
+}
